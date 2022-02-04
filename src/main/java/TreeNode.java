b4b89c8debfecd7eeb97f56aa28e450198ed6238
Dave Kosmal <dave.kosmal@malbek.io>
2022-02-04 12:05:44 -0800

    Improvements

--- src/main/java/TreeNode.java
+++ src/main/java/TreeNode.java
@@ -2,7 +2,9 @@
  * A simple binary tree with String values.
  */
 public class TreeNode {
-    private String contents;
+
+    private final String contents; // Cannot be null
+
     private TreeNode left;
     private TreeNode right;
 
@@ -10,18 +12,25 @@ public class TreeNode {
      * Performs a deep comparison with another TreeNode.
      */
     public boolean deepCompare(TreeNode tn2) {
-
-        return false; // TODO: Implement
+        return this.contents.equals(tn2.contents) &&
+                (((this.left == null) && (tn2.left == null)) ||
+                        (((this.left != null)) && this.left.deepCompare(tn2.left))) &&
+                (((this.right == null) && (tn2.right == null)) ||
+                        (((this.right != null)) && this.right.deepCompare(tn2.right)));
     }
 
     // -----------------------------------------------------------------------
 
     public TreeNode(String contents) {
+        // Do not allow null value
+        if (contents == null) {
+            throw new IllegalArgumentException("Contents cannot be null");
+        }
         this.contents = contents;
     }
 
     public TreeNode(String contents, TreeNode left, TreeNode right) {
-        this.contents = contents;
+        this(contents);
         this.left = left;
         this.right = right;
     }
@@ -30,23 +39,11 @@ public class TreeNode {
         return left;
     }
 
-    public void setLeft(TreeNode left) {
-        this.left = left;
-    }
-
     public TreeNode getRight() {
         return right;
     }
 
-    public void setRight(TreeNode right) {
-        this.right = right;
-    }
-
     public String getContents() {
         return contents;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
     }
 }
